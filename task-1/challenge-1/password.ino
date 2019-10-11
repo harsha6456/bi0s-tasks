113789f414386e0eb0e2483d6a1984dd4422b6f7
programmerAI <56351627+harsha6456@users.noreply.github.com>
2019-10-11 12:45:14 +0530

    Update password.ino

--- task-1/challenge-1/password.ino
+++ task-1/challenge-1/password.ino
@@ -3,14 +3,14 @@
 void setup() 
 {
   Serial.begin(9600);
-  String password= String ("whatisthematter");      
+  String password= String ("i will change password");      
 
 }
 
 
 void loop() {
   Serial.println("Enter the password");    
-  String password= String ("whatisthematter");      
+  String password= String ("i will change password");      
   String a= Serial.readString();
   if (a!= password) 
     {
